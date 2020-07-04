143981de805d168cf9132ef42f050cd42507f028
SidK <sidcool1234@gmail.com>
2020-07-04 12:48:57 +0530

    Introduces Lombok in the project and commons lang apache

--- src/main/java/com/sid/practice/streams/models/Dish.java
+++ src/main/java/com/sid/practice/streams/models/Dish.java
@@ -1,5 +1,10 @@
 package com.sid.practice.streams.models;
 
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
 public class Dish {
     private final String name;
     private final int calories;
