c22116aabe95ff44a4b31a2c29b10d46977db8b6
SidK <sidcool1234@gmail.com>
2020-07-04 13:12:22 +0530

    Introduces parallel tests

--- src/main/java/com/sid/practice/streams/StreamsExamples.java
+++ src/main/java/com/sid/practice/streams/StreamsExamples.java
@@ -10,15 +10,29 @@ public class StreamsExamples {
 
     public static String[] getDishNamesWithLowerCaloriesThan(int caloriesLimit, List<Dish> dishes) {
         if (dishes == null || dishes.size() == 0) {
-            dishes = ImmutableList.of(
-                    new Dish("Pasta", 1200, 34),
-                    new Dish("Cake", 1000, 22),
-                    new Dish("Salad", 120, 45),
-                    new Dish("Greens", 130, 47));
+            dishes = defaultDishes();
         }
 
         return dishes.stream().filter( dish -> dish.getCalories() < caloriesLimit)
                               .sorted(Comparator.comparing(Dish::getCalories))
                               .map(Dish::getName).toArray(String[]::new);
+    }
+
+    public static String[] getDishNamesWithLowerCaloriesThanPar(int caloriesLimit, List<Dish> dishes) {
+        if (dishes == null || dishes.size() == 0) {
+            dishes = defaultDishes();
+        }
+
+        return dishes.parallelStream().filter( dish -> dish.getCalories() < caloriesLimit)
+                .sorted(Comparator.comparing(Dish::getCalories))
+                .map(Dish::getName).toArray(String[]::new);
+    }
+
+    private static ImmutableList<Dish> defaultDishes() {
+        return ImmutableList.of(
+                new Dish("Pasta", 1200, 34),
+                new Dish("Cake", 1000, 22),
+                new Dish("Salad", 120, 45),
+                new Dish("Greens", 130, 47));
     }
 }
--- src/test/java/com/sid/practice/streams/StreamsExamplesTest.java
+++ src/test/java/com/sid/practice/streams/StreamsExamplesTest.java
@@ -3,11 +3,13 @@ package com.sid.practice.streams;
 
 import com.github.javafaker.Faker;
 import com.sid.practice.streams.models.Dish;
+import org.apache.commons.lang3.time.StopWatch;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 class StreamsExamplesTest {
 
@@ -17,10 +19,30 @@ class StreamsExamplesTest {
         Assertions.assertArrayEquals(new String[]{"Salad", "Greens"}, dishNamesWithLowerCaloriesThan);
     }
 
+    @Test
+    public void performanceTest(){
+        StopWatch watch = new StopWatch();
+        List<Dish> dishes = getRandomDishes();
+        watch.start();
+        StreamsExamples.getDishNamesWithLowerCaloriesThan(500, dishes);
+        watch.stop();
+        System.out.println("Time taken == "+ watch.getNanoTime());
+
+
+
+        StopWatch watch1 = new StopWatch();
+        dishes = getRandomDishes();
+        watch1.start();
+        StreamsExamples.getDishNamesWithLowerCaloriesThanPar(500, dishes);
+        watch1.stop();
+        System.out.println("Time taken == "+ watch1.getNanoTime());
+
+    }
+
     private List<Dish> getRandomDishes(){
         Faker f = new Faker();
         List<Dish> dishes = new ArrayList<>();
-        for(int i=0;i<100;i++) {
+        for(int i=0;i<100000;i++) {
             String dishName = f.letterify("a??efghij???opqrstuvw??yz");
             int calories = Integer.parseInt(f.numerify("###0"));
             int price = calories * 2;
